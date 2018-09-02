6899a43d071e3c89ebb22db6a6e544b7b9ec8eb6
Manu <manuhazen@outlook.com>
2018-09-02 14:21:53 -0400

    Style: Site almost finished

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,39 @@
-$(function () {
-  console.log('DOM is ready. Write some jQuery or whatever...')
-})
+$(function() {
+  $('.toggle-nav').click(function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    toggleNav();
+  });
+
+  $(window).scroll(function(){
+    scrollFunction();
+  })
+});
+
+
+function toggleNav() {
+  if ($('#app-wrapper').hasClass('show-nav')) {
+    $('#app-wrapper').removeClass('show-nav');
+  } else {
+    $('#app-wrapper').addClass('show-nav');
+  }
+}
+
+function scrollFunction() {
+  var firstSection = $('.section.first').height();
+  console.log(firstSection);
+  if ($(this).scrollTop() > firstSection) {
+    $('.c-nav').addClass('show');
+  } else {
+      $('.c-nav').removeClass('show');
+  }
+
+}
+
+$(document).keyup(function() {
+  if (e.keyCode == 27) {
+    if ($('#app-wrapper').hasClass('show-nav')) {
+      toggleNav();
+    }
+  } 
+  });
